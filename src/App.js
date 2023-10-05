013e6e2579709a41006667e8bdd9cf5fb7a99a53
Mike James <mikejames386@gmail.com>
2023-10-05 15:58:19 -0500

    add bootsrap imports and basic top header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [records, setRecords] = useState([])
@@ -14,27 +18,25 @@ function App() {
     try {
       const response = await axios.get('http://127.0.0.1:5000/record');
       setRecords(response.data);
-    } catch {
-      console.log('failed');
+    } catch (e) {
+      console.log(e);
     }
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">My Record App</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav.Link href="#">Sign In</Nav.Link>
+            {/* <Navbar.Text> */}
+            {/*   Signed in as: <a href="#login">Mark Otto</a> */}
+            {/* </Navbar.Text> */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
