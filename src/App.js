1caa9f5dadef7318b104281fcd77bb931922121a
Mike James <mikejames386@gmail.com>
2023-10-09 16:19:17 -0500

    remove unused function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { fetchRecords } from './utils/server.js'
 
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
